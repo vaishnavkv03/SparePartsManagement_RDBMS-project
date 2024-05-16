cf9ce4bf3f3a1a4ed7af62ed86247cc794c19c18
sreekanthch1729 <137896394+sreekanthch1729@users.noreply.github.com>
2024-05-16 23:38:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function navigateTo(page) {
+    window.location.href = page;
+}
+
+function showPopup(message) {
+    alert(message);
+}
+
+function initiateLogout() {
+    alert('You have been logged out.');
+    // Implement actual logout logic here
+}
