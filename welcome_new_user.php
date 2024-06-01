1b66b650cb52a884346d262adeec52b66294ea94
Sreehari0o4 <SreehariwrKo8@gmail.com>
2024-06-01 20:08:50 +0530

    Add welcome new user page

--- welcome_new_user.php
+++ welcome_new_user.php
@@ -2,10 +2,11 @@
 <html>
 <head>
     <title>Welcome</title>
+    <link rel="stylesheet" href="admin_styles.css">
 </head>
 <body>
     <h2>Welcome, New User!</h2>
     <p>Your registration was successful.</p>
-    <p><a href="login.php">Login here</a> to access your account.</p>
+    <p><a href="admin_login.php">Login here</a> to access your account.</p>
 </body>
 </html>
