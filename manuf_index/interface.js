7be4d965b111e45212b835e4eddad572a02057f0
Sreehari0o4 <150273737+Sreehari0o4@users.noreply.github.com>
2024-06-20 10:24:20 +0530

    Add manuf_index files

---
+++ manuf_index/interface.js
@@ -0,0 +1,8 @@
+function goToLoginPage() {
+    window.location.href = '../index/index.php'; // Replace 'home.html' with the actual URL of your home page
+  }
+  
+  function redirectToOrders() {
+    window.location.href = "../manuf_orders/manu_orders.php"; // Replace with the URL of your orders management page
+}
+
---
+++ manuf_index/manuf_index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Manufacturer Window</title>
+<link rel="stylesheet" href="interface.css">
+</head>
+<body>
+    <div class="header">
+        <div class="logo">SPAREPARTS</div>
+        <div class="links">           
+               
+        </div>
+    </div>
+
+<div class="container">
+    
+    <div class="heading">
+        WELCOME <br>
+        MANUFACTURER.
+    </div>
+    <div class="t">Find your orders here</div>
+    <button onclick="redirectToOrders()" style="--c:#E95A49">ORDERS</button> <br><br>
+    
+</div>
+<div class="top-bar">
+    <div class="left-content">
+        <!-- Other content like logo or title -->
+         
+    </div>
+    <button onclick="goToLoginPage()">LOGOUT</button>
+</div>
+
+
+
+<script src="interface.js"></script>
+</body>
+</html>
